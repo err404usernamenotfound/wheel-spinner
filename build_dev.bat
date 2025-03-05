5ecf3b95981b2ba9e0610b839fe7be41e7c5fce5
Deniel Ramirez <ramirez2021129@mkt.ceu.edu.ph>
2025-03-05 11:12:04 +0800

    use the legacy OpenSSL provider

--- build_dev.bat
+++ build_dev.bat
@@ -1,2 +1,10 @@
+@echo off
+
+REM Remove the existing dist directory
 rmdir /s /q dist
+
+REM Set the NODE_OPTIONS environment variable to use the legacy OpenSSL provider
+set NODE_OPTIONS=--openssl-legacy-provider
+
+REM Run the Webpack build
 call npx webpack --config build/dev.config.js
